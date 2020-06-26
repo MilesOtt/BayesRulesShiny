c025732db571fb5f9d22523a154aed001e77f4a6
tongantopdog <elaonalemoto@gmail.com>
2020-06-26 10:54:53 -0400

    Making improvements to Ch.2.1.5

--- Ch_2_Posterior/server.R
+++ Ch_2_Posterior/server.R
@@ -1,7 +1,6 @@
 library(shiny)
 library(tidyverse)
 
-
 server<-function(input, output){
   output$plot<-renderPlot({
     # Define possible articles
@@ -9,9 +8,18 @@ server<-function(input, output){
     # Define the prior model
     prior <- c(0.6, 0.4)
     article_sim <- sample_n(article,
-                            size = input$num, weight = prior, replace = TRUE)
-    ggplot(article_sim, aes(x=type))+
-      geom_bar()
+      size = input$num,weight = prior, replace = TRUE)
+    article_sim<-article_sim %>% 
+      count(type) %>% 
+      group_by(type) %>% 
+      mutate(prop=n/input$num)
+    ggplot(article_sim, aes(x=type, y=prop))+
+      geom_col()+
+      labs(title = "Posterior Distribution", x="Type", y="Percentage")+
+      theme(axis.text=element_text(size=14),
+           axis.title=element_text(size=14
+                                   ,face="bold"),
+           plot.title = element_text(size=22))
     
   })
   
--- Ch_2_Posterior/ui.R
+++ Ch_2_Posterior/ui.R
@@ -1,8 +1,39 @@
 library(shiny)
 set.seed(84735)
 
+
+
 ui<-fluidPage(
-  titlePanel("Section 2.1.5: Posterior Simulation"),
+  titlePanel("Chapter 2.1.5: Posterior Simulation"),
+  mainPanel(
+    p("Recall from Chapter 2 where we were
+interested in distinguishing fake news articles 
+      from real news articles. After looking through our 
+      sample of 150 articles, we found our prior probability 
+      to be 40% for fake articles and 60% for real articles. 
+      Now in this section, we are interested in providing a 
+      connection between articles posted online and their corresponding
+      probabilites. To start, we created a dataframe for types of articles;
+real and fake, and defined our prior model which is mentioned earlier
+0.6 and 0.4. Since we want to simulate articles we might see on social 
+media, we are randomly sampling articles from the our article dataframe 
+      with the added weights of our priors.")
+  ),
   sliderInput("num", "Sample Number", value=0, min=10, max=10000),
-  plotOutput("plot")
-)
+  mainPanel(
+    p("The slider above changes the number of random samples we take 
+    given our types of articles and their prior probability. Below is a
+      frequency plot of each article. What do you notice as n increases?")
+  ),
+  plotOutput("plot"),
+  mainPanel(
+    br(),
+    br(),
+    br(),
+    br(),
+    br(),
+      p("This application was made by Elaona Lemoto in 
+        supplement to Bayes Rules! by Miles Ott, Mine Dogucu, and ---- . Bayes Rules!
+        is a textbook for undergarduates learning Bayesian statistics. ")
+  )
+  )
