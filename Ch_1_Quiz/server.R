808c69397b3d442149d3233cd74427e36747334f
tongantopdog <elaonalemoto@gmail.com>
2020-06-26 13:51:00 -0400

    Trying to update reactive function; not working well. Also, still trying to figure out how to make columns.

--- Ch_1_Quiz/server.R
+++ Ch_1_Quiz/server.R
@@ -1,18 +1,22 @@
 library(shiny)
 
 server<-function(input, output, session) {
-  output$answer<-renderText({
-    asInt1=as.integer(input$q1)
-    asInt2=as.integer(input$q2)
-    asInt3=as.integer(input$q3)
-    asInt4=as.integer(input$q4)
-    x=asInt1+asInt2+asInt3+asInt4
-    if(x<=5){
-      paste("Your current thinking are fairly frequentist!");
+  observeEvent(input$Submit,{ 
+ output$answer<-renderPrint({ 
+     asInt1=as.integer(input$q1)
+     asInt2=as.integer(input$q2)
+     asInt3=as.integer(input$q3)
+     asInt4=as.integer(input$q4)
+     x=asInt1+asInt2+asInt3+asInt4
+   if(x<=5){
+      "Your current thinking are fairly frequentist!";
     } else if(x>=9){
-      paste("According to your answers, you already think like a Bayesian!");
+      "According to your answers, you already think like a Bayesian!";
     } else {
-      paste("You don't take sides; you shared ideas from frequentists and Bayesians")
+      "You don't take sides; you shared ideas from frequentists and Bayesians"
     }
-  })
+})
+ })
+  
+
 }
