06bf6f54ae290d0e94dcedbf4096558a11d511f8
tongantopdog <elaonalemoto@gmail.com>
2020-06-27 14:42:55 -0400

    fixed the rows!

--- Ch_1_Quiz/server.R
+++ Ch_1_Quiz/server.R
@@ -1,7 +1,6 @@
 library(shiny)
 
 server<-function(input, output, session) {
-  observeEvent(input$Submit,{ 
  output$answer<-renderPrint({ 
      asInt1=as.integer(input$q1)
      asInt2=as.integer(input$q2)
@@ -12,11 +11,12 @@ server<-function(input, output, session) {
       "Your current thinking are fairly frequentist!";
     } else if(x>=9){
       "According to your answers, you already think like a Bayesian!";
+    } else if(x<=8 |x>=6){
+      "You don't take sides; you shared ideas from frequentists and Bayesians";
     } else {
-      "You don't take sides; you shared ideas from frequentists and Bayesians"
+      "0 ";
     }
-})
- })
-  
+
+    })
 
 }
--- Ch_1_Quiz/ui.R
+++ Ch_1_Quiz/ui.R
@@ -4,34 +4,40 @@ ui<-fluidPage(
   titlePanel("What type of Statistician are you?"),
   #setting  up radio buttons
   mainPanel(
-    p("Take this quiz to see whether you share more frequentist ideas or 
-bayesian ideas,or both!")
-  ),
+    p("Take this quiz to see whether you share more frequentist ideas,
+bayesian ideas, or both!"),
+    br()
+    ),
   fluidRow(
-    column(4,
-  radioButtons("q1", "When flipping a coin, we say that 'the probability of flipping heads is 0.5'. 
+    column(6, 
+           radioButtons("q1", "When flipping a coin, we say that 'the probability of flipping heads is 0.5'. 
                How do you interpret this probability?",choices=list("If I flip this coin over and over, 
-                roughly 50% will be Heads."=1, "Heads and Tails are equally plausible."=3,
-                                                      "Both a and b make sense."=2),selected=NULL),
-  radioButtons("q2", "A meterologist warns that 'there's a 0.1 probability
-               of rain today.' How do you interpret this probability?",
-               choices =list("If we observe today over and over, it will rain on roughly 10% of todays.
-                             "=1,"It's equally likely to rain or not rain."=3,
-                             "The meteorologist's calculation is wrong. It will either rain or not rain, 
-                             thus the probability of rain can only be 0 or 1"=1),selected=1)),
-  column(4,
-  radioButtons("q3", "Consider two claims. (1) Zuofu claims that he can predict the out- come of a coin flip. To test his claim, you flip a fair coin 10 times and he correctly predicts all 10! (2) Kavya claims that she can dis- tinguish Dunkin’ Donuts1 coffee from Starbucks coffee. To test her claim, you give her 10 coffee samples and she correctly identifies the origin of each! In light of these experiments, what do you conclude? ", choices = list("You’re more confident in Kavya’s claim than Zuofu’s claim."=3,"The evidence supporting Zuofu’s claim is just as strong as the
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                     evidence supporting Kavya’s claim."=1), selected=1),
-  radioButtons("q4","Suppose that during a recent doctor’s visit, you tested positive for a very rare disease. 
-               If you only get to ask the doctor one question, which would it be?", 
-               choices= list("What’s the chance that I actually have the disease?"=3,
-                             "If in fact I don’t have the disease, what’s the chance that 
-                             I would’ve gotten this positive test result?"=1),selected=1))),
+                                                                    roughly 50% will be Heads."=1, "Heads and Tails are equally plausible."=3,
+                                                                    "Both a and b make sense."=2),selected=0),
+             radioButtons("q2", "A meterologist warns that 'there's a 0.1 probability
+                          of rain today.' How do you interpret this probability?",
+                          choices =list("If we observe today over and over, it will rain on roughly 10% of todays.
+                                        "=1,"It's equally likely to rain or not rain."=3,
+                                        "The meteorologist's calculation is wrong. It will either rain or not rain, 
+                                        thus the probability of rain can only be 0 or 1"=1),selected=0)),
+    column(6, 
+            radioButtons("q3", "Consider two claims. (1) Zuofu claims that he 
+can predict the out- come of a coin flip. To test his claim, you flip a 
+                           fair coin 10 times and he correctly predicts all 10! (2) Kavya claims that 
+                           she can dis- tinguish Dunkin’ Donuts1 coffee from Starbucks coffee. To test 
+                           her claim, you give her 10 coffee samples and she correctly identifies the 
+                           origin of each! In light of these experiments, what do you conclude? ", 
+                           choices = list("You’re more confident in Kavya’s claim than Zuofu’s claim."
+                                          =3,"The evidence supporting Zuofu’s claim is just as strong as the
+                                          evidence supporting Kavya’s claim."=1), selected=0),
+           radioButtons("q4","Suppose that during a recent doctor’s visit, you tested positive for a very rare disease. 
+                        If you only get to ask the doctor one question, which would it be?", 
+                        choices= list("What’s the chance that I actually have the disease?"=3,
+                                      "If in fact I don’t have the disease, what’s the chance that 
+                                      I would’ve gotten this positive test result?"=1),selected=0))),
+
   verbatimTextOutput("null"),
   submitButton("Submit"),
+verbatimTextOutput("answer")
 
-  
-  verbatimTextOutput("answer")
-  
-  )
-
+)
