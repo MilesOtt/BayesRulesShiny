5b6bb98596de02e12d90ae35362bcfda54394eff
tongantopdog <elaonalemoto@gmail.com>
2020-07-02 00:16:26 -0400

    fixing app

--- Ch_2_Posterior/server.R
+++ Ch_2_Posterior/server.R
@@ -9,19 +9,84 @@ server<-function(input, output){
     prior <- c(0.6, 0.4)
     article_sim <- sample_n(article,
       size = input$num,weight = prior, replace = TRUE)
-    article_sim<-article_sim %>% 
-      count(type) %>% 
-      group_by(type) %>% 
-      mutate(prop=n/input$num)
-    ggplot(article_sim, aes(x=type, y=prop))+
-      geom_col()+
-      labs(title = "Posterior Distribution", x="Type", y="Percentage")+
+    #First Plot
+    ggplot(article_sim, aes(x=type))+
+      geom_bar()+
+      labs(title = "Prior Distribution", x="Type", y="Count")+
       theme(axis.text=element_text(size=14),
            axis.title=element_text(size=14
                                    ,face="bold"),
            plot.title = element_text(size=22))
+})
     
-  })
+    
+    
+output$plot2<-renderPlot({
+   article <- data.frame(type = c("real", "fake"))
+   # Define the prior model
+   prior <- c(0.6, 0.4)
+   article_sim <- sample_n(article,
+                           size = input$num,weight = prior, replace = TRUE)
+   #Likelihood 
+   article_sim <- article_sim %>% 
+     mutate(likelihood = 
+              case_when(type == "fake" ~ 0.28,type == "real" ~ 0.02 ))
+   #Creting Proportions
+   data <- c("no", "yes")
+   # Simulate exclamation point usage
+   set.seed(3)
+   article_sim <- article_sim %>%
+     group_by(1:n()) %>%
+     mutate(usage = sample(data, size = 1,
+                           prob = c(1 - likelihood, likelihood))) 
+   
+   ggplot(article_sim, aes(x = usage)) + geom_bar() +
+     facet_wrap(~ type)    +
+     labs(title = "Likelihood Distribution", x="Usage", y="Count")+
+     theme(axis.text=element_text(size=14),
+           axis.title=element_text(size=14
+                                   ,face="bold"),
+           plot.title = element_text(size=22))
+   
+ })
+ 
+ 
+ 
+ 
+ 
+ 
+ output$plot3<-renderPlot({
+
+   article <- data.frame(type = c("real", "fake"))
+   # Define the prior model
+   prior <- c(0.6, 0.4)
+   article_sim <- sample_n(article,
+                           size = input$num,weight = prior, replace = TRUE)
+   #Likelihood 
+   article_sim <- article_sim %>% 
+     mutate(likelihood = 
+              case_when(type == "fake" ~ 0.28,type == "real" ~ 0.02 ))
+   #Creting Proportions
+  
+   
+   data <- c("no", "yes")
+   set.seed(3)
+   article_sim <- article_sim %>%
+     group_by(1:n()) %>%
+     mutate(usage = sample(data, size = 1,
+                           prob = c(1 - likelihood, likelihood))) 
+   
+  
+   ggplot(article_sim, aes(x = type)) + 
+     geom_bar() +
+     facet_wrap(~ usage)+
+     labs(title = "Posterior Distribution", x="Type", y="Count")+
+     theme(axis.text=element_text(size=14),
+           axis.title=element_text(size=14
+                                   ,face="bold"),
+           plot.title = element_text(size=22))
+ })
+
   
   
 }
--- Ch_2_Posterior/ui.R
+++ Ch_2_Posterior/ui.R
@@ -4,36 +4,67 @@ set.seed(84735)
 
 
 ui<-fluidPage(
-  titlePanel("Chapter 2.1.5: Posterior Simulation"),
-  mainPanel(
-    p("Recall from Chapter 2 where we were
-interested in distinguishing fake news articles 
-      from real news articles. After looking through our 
-      sample of 150 articles, we found our prior probability 
-      to be 40% for fake articles and 60% for real articles. 
-      Now in this section, we are interested in providing a 
-      connection between articles posted online and their corresponding
-      probabilites. To start, we created a dataframe for types of articles;
-real and fake, and defined our prior model which is mentioned earlier
-0.6 and 0.4. Since we want to simulate articles we might see on social 
-media, we are randomly sampling articles from the our article dataframe 
-      with the added weights of our priors.")
+  titlePanel(
+    h1("Chapter 2.1.5: Posterior Simulation with Article Verification", align = "center")
   ),
+    # Sidebar to demonstrate various slider options ----
+    sidebarPanel(
+      
+   
   sliderInput("num", "Random Sample Number", value=0, min=1, max=10000),
+    h6("The slider above changes the number of random samples based 
+on our weights for real and fake articles." 
+),
+style = "position:fixed;width:inherit;",
+"Inputs"),
+  
   mainPanel(
-    p("The slider above changes the number of random samples we take 
-    given our types of articles and their prior probability. Below is a
-      frequency plot of each article. What do you notice as n increases?")
+    div(
+      strong("Recall from Chapter 2 where we were
+interested in distinguishing fake news articles from real news articles. 
+         After looking through our sample of 150 articles, 
+         we found our prior probability to be 40% for fake articles and 60% for real 
+         articles.  In this activity, we are interested in providing a connection 
+         between articles posted online and their corresponding probabilites.
+         To start, we have a dataframe for types of articles;
+         real and fake, and defined our prior model as the probabilities
+         found before. Since we want to simulate articles we might see on social 
+         media, we are randomly sampling articles from the our article dataframe with
+         their weighted probabilities. Throughout this exercise, you will look
+             at the different distributions and below as you change the number
+             or random samples "),
+      style = "padding-right: 5%; padding-left: 5%"
+      ),
+    br(),
+    div(
+    p("Now in our analysis of real and fake articles, we created a likelihood 
+      variable tying together exclamation usage and an articles chance of being 
+real or fake. In this case, we defined any article with an exclamation mark 
+to have a 28% chance of being fake and 2% of being real.
+To better visualize this simulation, below is our prior distribution given
+our random sample numbers. Confirm that the prior distribution 
+is similar to the the 
+      weights that we defined earlier."),
+    style = "padding-right: 5%; padding-left: 5%"),
+
+    div(
+      plotOutput('plot'),
+      style = "padding-right: 10%; padding-left: 10%"),
+    div(
+      p("This here is the likelihood distribution."),
+      style = "padding-right: 5%; padding-left: 5%"),
+  div(
+        plotOutput('plot2'),
+        style = "padding-right: 10%; padding-left: 10%"
+      ),
+  div(
+    plotOutput('plot3'),
+    style = "padding-right: 10%; padding-left: 10%"
   ),
-  plotOutput("plot"),
-  mainPanel(
-    br(),
-    br(),
-    br(),
-    br(),
-    br(),
+  br(),
+  br(),
+  br(),
       p("This application was made by Elaona Lemoto in 
-        supplement to Bayes Rules! by Miles Ott, Mine Dogucu, and ---- . Bayes Rules!
-        is a textbook for undergarduates learning Bayesian statistics. ")
-  )
+        supplement to Bayes Rules! by  Mine Dogucu, Alicia Johnson, and Miles Ott. Bayes Rules!
+        is a textbook for undergarduates learning Bayesian statistics. "))
   )
