7e16fa4cce5a46131e9f89d09cee3a8a3fc2c020
Miles <miles_ott@alumni.brown.edu>
2020-08-19 09:56:51 -0400

    worked on chapter 2 #16

--- Ch_1_Quiz/ui.R
+++ Ch_1_Quiz/ui.R
@@ -121,15 +121,15 @@ tabPanel("Chapter 2: Posterior",
            h1("Chapter 2.1.5: Posterior Simulation with Article Verification", align = "center")
          ),
          sidebarPanel(
-           "The slider changes the number of random samples we take of articles overall.",
+           "The slider changes the number of articles we will simulate.",
 
-           sliderInput("c2num", "Number of Random Samples", value=50, min=1, max=500),
+           sliderInput("c2num", "Number of Simulated Articles", value=50, min=1, max=500),
 
-           "Our prior is that 40% articles are fake and 60% of our articles are real. Now 
-let's talk about what's in our data. Below, you can alter the data observations for type of articles that have an exclamation point
-           in their title. Again, the default prior probabilities as they are in the book for articles that are fake and that have an
-           exclamation point is 28% and 2% of real news articles use exclamation points. Therefore, the values need to remain
-           between 0 and 1.",
+           "Our prior is that 40% articles are fake and 60% of our articles are real, so we simulated articles according to these prior probabilities. Now 
+let's talk about what's in our data. Next, we will simulate exclamation point usage. Below, you can alter the data observations for type of articles that have an exclamation point
+           in their title. In order to do that, we have to specify the likelihood that a fake news article uses exclamation points, and that real news articles use exclamation points. 
+           In the book, the we found from our data that the likelihood that a fake news article uses an exclamation point in the headline is 0.28, while the likelihood
+           that a real news article uses an exclamation point in the headline is 0.02. Here we let you change those likelihoods to see how that impacts the posterior.",
 
 
            numericInput("c2p1", "P(Exclamation Point | Fake Article ) ", value=0.28,min=0,max=1),
@@ -144,15 +144,11 @@ let's talk about what's in our data. Below, you can alter the data observations 
                     interested in distinguishing fake news articles from real news articles.
                     After looking through our sample of 150 articles,
                     we found our prior probability to be 40% for fake articles and 60% for real
-                    articles.  In this activity, we are interested in providing a connection
-                    between articles posted online and their corresponding probabilites.
-                    To start, we have a dataframe for types of articles;
-                    real and fake, and defined our prior model as the probabilities
-                    found before. Since we want to simulate articles we might see on social
-                    media, we are randomly sampling articles from the our article dataframe with
-                    their weighted probabilities. Throughout this exercise, you will look
-                    at the different distributions and below as you change the number
-                    or random samples "),
+                    articles.  In this activity, you will simulate the posterior probability that an article
+                    is fake given that the headline uses exclamation points. In the panel to the left you can 
+                    choose how many articles to simulate, and specify different likelihoods. Does the number of simulations
+                    impact the posterior? How does the likelihood impact the posterior?
+                     "),
       
              #This is what we observed in the data
              style = "padding-right: 5%; padding-left: 5%"
@@ -160,10 +156,8 @@ let's talk about what's in our data. Below, you can alter the data observations 
            br(),
            div(
              p("
-               To better visualize this simulation, below is our prior distribution given
-               our random sample numbers. Confirm that the prior distribution
-               is similar to the the
-               weights that we defined earlier."),
+               Here is our simulated prior distribution based on the number of simulations you specified.
+               Is the simulated prior distribution matching up with the prior we specified?"),
              style = "padding-right: 5%; padding-left: 5%"),
 
            div(
@@ -172,15 +166,9 @@ let's talk about what's in our data. Below, you can alter the data observations 
            div(
            
 
-             p("Now in our analysis of real and fake articles, we created a likelihood
-               variable tying together exclamation point usage and types of articles.
-               As our default inputs, we are using the data on exclmation points and types of
-               articles that we gathered earlier in Chapter 2. Recall that we found a
-               28% chance of an articles title having an exclamtion point given it's fake
-               and a 2% chance of having an exclamation point given it's a real article.
-               To see how the likelihood distribution
-               and the posterior distribution would alter with different data,
-               change the probabilities in the right column. In this case, what is the likelihood
+             p("Next, we simulate our likelihood. To see how the likelihood distribution
+               influences the posterior distribution, 
+               change the probabilities in the panel to the left. In this case, what is the likelihood
                of an exclamation point given fake news article and what is the likelihood
                of an article title using an exclamation point given it's a real news article?"),
              style = "padding-right: 5%; padding-left: 5%"),
@@ -189,9 +177,8 @@ let's talk about what's in our data. Below, you can alter the data observations 
              style = "padding-right: 10%; padding-left: 10%"
            ),
            div(
-             p("Finally, we have our posterior distribution. The graph below shows the proportion of
-               real and fake articles with exclamation point usage. Based on the data we input, the
-               posterior distribution as shown in the graph below will change. "),
+             p("Finally, we have our posterior distribution. The graph below shows the simulated proportion of
+               real and fake articles with exclamation point usage. "),
              style = "padding-right: 5%; padding-left: 5%"),
            div(
              plotOutput('plot3'),
